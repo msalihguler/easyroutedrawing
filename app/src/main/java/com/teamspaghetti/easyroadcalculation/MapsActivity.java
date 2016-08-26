15b9bd540e2d3b1060d624d1dcb93efe96c00653
Salih <muhammedsalihguler@gmail.com>
2016-08-26 14:31:09 +0300

    Screenshots added

--- app/src/main/java/com/teamspaghetti/easyroadcalculation/MapsActivity.java
+++ app/src/main/java/com/teamspaghetti/easyroadcalculation/MapsActivity.java
@@ -3,6 +3,8 @@ package com.teamspaghetti.easyroadcalculation;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -32,19 +34,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         easyRouteCalculation = new EasyRouteCalculation(this,mMap);
         LatLng ankara = new LatLng(39.55,32.51);
         LatLng izmir = new LatLng(38.25,27.7);
-        final LatLng istanbul = new LatLng(41.0,28.58);
+        LatLng istanbul = new LatLng(41.0,28.58);
         LatLng antalya = new LatLng(36.52,30.45);
         LatLng mugla = new LatLng(37.15,28.22);
         LatLng samsun = new LatLng(41.15,36.22);
         LatLng sinop = new LatLng(42.,35.11);
-        easyRouteCalculation.gotoMyLocation(true);
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng));
-                easyRouteCalculation.calculateRouteFromMyLocation(latLng);
-            }
-        });
+
+        easyRouteCalculation.addLocation(ankara);
+        easyRouteCalculation.addLocation(izmir);
+        easyRouteCalculation.addLocation(istanbul);
+        easyRouteCalculation.addLocation(antalya);
+        easyRouteCalculation.addLocation(mugla);
+        easyRouteCalculation.addLocation(samsun);
+        easyRouteCalculation.addLocation(sinop);
+
+        easyRouteCalculation.calculateRouteForMultiplePositions();
     }
 
     @Override
